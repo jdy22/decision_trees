f1db3abf2e1b106254d7726378d8690ac812af70
Jo Ye <jdy15@ic.ac.uk>
2022-10-18 14:44:19 +0100

    Added node class structure.

---
+++ node_class.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self, split_attribute, split_value, left_child, right_child, is_leaf=False):
+        self.split_attribute = split_attribute
+        self.split_value = split_value
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = is_leaf
