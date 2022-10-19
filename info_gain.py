0c561d0e4215bdc1c502a099e67bc06e1f23b3a7
Jo Ye <jdy15@ic.ac.uk>
2022-10-19 13:21:07 +0100

    Entropy and info gain functions defined.

--- info_gain.py
+++ info_gain.py
@@ -1,5 +1,9 @@
+import numpy as np
+
 def calc_entropy(dataset):
     """ Calculate entropy of dataset.
+
+    Only works if dataset has more than one row.
 
     Args:
         dataset (np.array) : Nx8 array where N = number of samples, column 0 to 6 are attributes and 7 is label.
@@ -7,7 +11,13 @@ def calc_entropy(dataset):
     Returns:
         float : Entropy of dataset
     """
-    return None
+    labels = dataset[:,-1]
+    unique_labels, counts = np.unique(labels, return_counts=True)
+    total_count = np.sum(counts)
+    weights = counts/total_count
+    entropy = np.sum(-weights*np.log2(weights))
+
+    return entropy
 
 
 def calc_info_gain(parent_dataset, left_dataset, right_dataset):
@@ -22,4 +32,22 @@ def calc_info_gain(parent_dataset, left_dataset, right_dataset):
     Returns:
         float : Information gain of split
     """
-    return None
+    size_parent = len(parent_dataset)
+    size_left = len(left_dataset)
+    size_right = len(right_dataset)
+
+    entropy_parent = calc_entropy(parent_dataset)
+    entropy_left = calc_entropy(left_dataset)
+    entropy_right = calc_entropy(right_dataset)
+
+    info_gain = entropy_parent - (size_left/size_parent)*entropy_left - (size_right/size_parent)*entropy_right
+
+    return info_gain
+
+
+if __name__ == "__main__":
+    parent_dataset = np.array([[1, 2], [3, 2], [5, 4], [7, 4]])
+    left_dataset = parent_dataset[0:2, :]
+    right_dataset = parent_dataset[2:, :]
+    info_gain = calc_info_gain(parent_dataset, left_dataset, right_dataset)
+    print(info_gain)
