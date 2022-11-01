bce9ec639f9612ce656ffcf3930c823d3917428e
Priya <pj22@ic.ac.uk>
2022-11-01 15:03:00 +0000

    Adding comments to visualisation.py file

--- visualisation.py
+++ visualisation.py
@@ -1,3 +1,4 @@
+import numpy as np 
 import matplotlib
 import matplotlib.pyplot as plt
 from build_tree import decision_tree_learning
@@ -6,15 +7,36 @@ dataset = np.loadtxt("wifi_db/clean_dataset.txt")
 root_node = decision_tree_learning(dataset)
 
 def visualisation_label(node):
+    """Creat labels for the nodes in the tree visualisaton
+    
+    Args: 
+        node (Node) : Root node of trained decision tree
+
+    Returns: 
+        node_text_label (str) : The text label to be displayed at each node in the tree visualisation
+    """
     if node.is_leaf == False:
         split_threshold = node.split.threshold
         split_attribute_index = node.split.attribute_index 
-        output = f'X{split_attribute_index}<{split_threshold}'
+        node_text_label = f'X{split_attribute_index}<{split_threshold}'
     elif node.is_leaf == True:
-        output = f'Room : {node.label}'
-    return output
+        node_text_label = f'Room : {node.label}'
+    return node_text_label
 
 def tree_visual(node, tree, count, spread, drop, x, y):
+    """Creates tree visualisation
+
+    Args:
+        node (Node) : Root node of trained decision tree
+        tree (matplotlib subplot) : plot of tree visualisation 
+        count (int) : Keeps count of the layer of tree
+        spread (float) : The distance spread of the entire tree
+        drop (int): The distance drop between tree layers
+        x (int) : The x coorindate of the box location
+        y (int) : The y coorindate of the box location
+    Returns: 
+        tree : plot containing tree visualisation 
+    """
     if node.is_leaf == False: 
         tree.text(x,y, visualisation_label(node), color = "black", bbox = dict(facecolor="white", edgecolor = "black"))
         distance = spread/(2**count)
